c94625076a1ad7983918a7e1e67795d3be9b05e1
kiriltaran <kiriltaran@gmail.com>
2017-10-04 12:14:55 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 
   //menu burger
   $('.nav-icon').click(function () {
-    $('.nav-list').slideToggle();
+    $('.nav-list').slideToggle()
   });
 
   //progress circles
