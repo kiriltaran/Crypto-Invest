f136da7dc6c39971c16c8710d4046cdf6f95e3d5
kiriltaran <kiriltaran@gmail.com>
2017-10-11 21:07:37 +0300

    fix

--- js/signin.js
+++ js/signin.js
@@ -1,45 +1,43 @@
 $(document).ready(function () {
 
-    $("#phone").intlTelInput({
-        initialCountry: "auto",
-        autoPlaceholder: true,
-        separateDialCode: false,
-        autoHideDialCode: false,
-        formatOnDisplay: false,
-        nationalMode: false,
-        geoIpLookup: function (callback) {
-            $.get('https://ipinfo.io', function () {
-            }, "jsonp").always(function (resp) {
-                var countryCode = (resp && resp.country) ? resp.country : "";
-                callback(countryCode);
-            });
+  $("#phone").intlTelInput({
+    initialCountry: "auto",
+    autoPlaceholder: true,
+    separateDialCode: false,
+    autoHideDialCode: false,
+    formatOnDisplay: false,
+    nationalMode: false,
+    geoIpLookup: function (callback) {
+      $.get('https://ipinfo.io', function () {}, "jsonp").always(function (resp) {
+        var countryCode = (resp && resp.country) ? resp.country : "";
+        callback(countryCode);
+      });
+    }
+  });
 
-        },
-        utilsScript: "../libs/intl-tel-input/build/js/utils.js" // just for formatting/placeholders etc
-    });
+  $("#signIn").click(function () {
+    $(".first-step").hide();
+    $(".step-signIn, #backward").show();
+  });
 
-    $("#signIn").click(function () {
-        $(".first-step").hide();
-        $(".step-signIn, #backward").show();
-    });
+  $("#registration").click(function () {
+    $(".first-step").hide();
+    $(".step-registretion, #backward").show();
+  });
 
-    $("#registration").click(function () {
-        $(".first-step").hide();
-        $(".step-registretion, #backward").show();
-    });
+  $("#backward").click(function () {
+    $(this).hide();
+    $(".step-registretion, .step-signIn").hide();
+    $(".first-step").show();
+  });
 
-    $("#backward").click(function () {
-        $(this).hide();
-        $(".step-registretion, .step-signIn").hide();
-        $(".first-step").show();
+  //get country phone codes
+  $.getJSON('json/countries.json', function (json, textStatus) {
+    $.each(json, function (index, val) {
+      leadCountryId[val.id] = val.name;
+      leadCountryId[val.id].dataCode = val.dataCode;
+      $('#country-ru').append('<option value=' + val.id + '>' + val.name.ru + '</option>');
+      $('#country-en').append('<option value=' + val.id + '>' + val.name.en + '</option>');
     });
-
-    //get country phone codes
-    $.getJSON('json/countries.json', function (json, textStatus) {
-        $.each(json, function (index, val) {
-            leadCountryId[val.id] = val.name;
-            leadCountryId[val.id].dataCode = val.dataCode;
-            $('#country').append('<option value=' + val.id + '>' + val.name.ru + '</option>');
-        });
-    });
+  });
 });
